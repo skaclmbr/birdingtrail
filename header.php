c1d9396ee14b0b3d820480384c9b57cc6acf1fa4
Scott Anderson <ncbirdconservation@gmail.com>
2018-03-22 09:23:54 -0400

    working header, popup, distance to site

--- header.php
+++ header.php
@@ -12,21 +12,17 @@
 
   <body <?php body_class(); ?>>
 
-    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <div class="cover-container d-flex h-100 mx-auto flex-column">
+    <!--<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">-->
       <header class="masthead mb-auto">
         <div class="inner">
 
-          <h3 class="masthead-brand"><?php bloginfo('name'); ?></h3>
+          <a href="../"><h3 class="masthead-brand"><?php bloginfo('name');?></h3></a>
+          <!--<a href="<?php get_bloginfo('url'); ?>"><img class="header_logo" src="/wp-content/themes/birdingtrail/img/header_logo.png"></a>-->
           <?php wp_nav_menu( array( 
             'theme_location' => 'header-menu', 
             'menu_class' => 'nav nav-masthead justify-content-center list-inline' 
           ) ); ?>
-          <!-- STOCK BOOTSTRAP NAV
-          <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Contact</a>
-          </nav>
-        -->
+
         </div>
       </header>
