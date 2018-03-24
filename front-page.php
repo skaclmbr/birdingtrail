10682060039461368a5c5f26cf11556b1c698f00
Scott Anderson <ncbirdconservation@gmail.com>
2018-03-24 17:31:04 -0400

    basic, works, but can't add content from WP interface

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Template Name: Front Page
+ *
+ * The template for the page builder full-width.
+ *
+*/
+
+get_header(); ?>
+
+
+      <main role="main" class="inner front-page">
+        <a href="<?php home_url() ?>"><img class="header_logo" src="/wp-content/themes/birdingtrail/img/header_logo.png"></a>
+
+        <p class="lead">From our vast coastline to our high mountain peaks, North Carolina is home to a diversity of habitats that support hundreds of species of birds and other wildlife. Our unique geographic setting along the Atlantic coast flyway provides both breeding and wintering grounds for many birds, making North Carolina a premiere birding destination.</p>
+        <p class="lead">
+          <a href="/sites" class="btn btn-lg btn-secondary">Explore the Sites</a>
+        </p>
+        </main>
+
+<?php get_footer(); ?>
