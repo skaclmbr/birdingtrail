9690ca344a0e773800c7d1dc03b27eca81f69583
Scott Anderson <ncbirdconservation@gmail.com>
2018-04-27 09:30:52 -0400

    enable correct display of pages (that aren't lists of posts)

--- front-page.php
+++ front-page.php
@@ -10,13 +10,31 @@
 get_header(); ?>
 
 
-      <main role="main" class="inner front-page">
-        <a href="<?php home_url() ?>"><img class="header_logo" src="/wp-content/themes/birdingtrail/img/header_logo.png"></a>
+<main role="main" class="container">
 
-        <p class="lead">From our vast coastline to our high mountain peaks, North Carolina is home to a diversity of habitats that support hundreds of species of birds and other wildlife. Our unique geographic setting along the Atlantic coast flyway provides both breeding and wintering grounds for many birds, making North Carolina a premiere birding destination.</p>
-        <p class="lead">
-          <a href="/sites" class="btn btn-lg btn-secondary">Explore the Sites</a>
-        </p>
-        </main>
+	<div class="row brand row-pad">
+		<div class="col-md-12 center-block"><a href="<?php home_url() ?>"><img class="front-page-logo" src="/wp-content/uploads/2018/04/ncbt_front_page.png"></a></div>
+	</div>	
+	<div class="row row-pad">
+		<div class="col-md-12  lead">
+			From our vast coastline to our high mountain peaks, North Carolina is home to diverse people, birds, and natural areas. Our unique place along the Atlantic Coast provides a variety of outdoor experiences.
+		</div>
+	</div>
+	<div class="row row-pad">
+		<div class="col-md-12  lead">
+			<a href="/sites" class="btn btn-lg btn-secondary">Explore North Carolina</a>
+		</div>
+	</div>
+	<div class="learn-more rounded">
+		<div class="row">
+			<div class="col-12 lead lead-label">Learn More</div>
+		</div>
+		<div class="row row-pad">
+			<div class="col center-block"><a href="/birdwatching"><i class="fa fa-binoculars fa-2x"></i></a><p class="fp-label">Birdwatching</p></div>
+			<div class="col center-block"><a href="/economy"><i class="fa fa-usd fa-2x"></i></a><p class="fp-label">Economy</p></div>
+			<div class="col center-block"><a href="/education"><i class="fa fa-leaf fa-2x"></i></a><p class="fp-label">Conservation</p></div>
+		</div>
+	</div>
+</main>
 
 <?php get_footer(); ?>
